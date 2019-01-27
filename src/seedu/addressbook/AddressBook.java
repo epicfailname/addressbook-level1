6956d098aacf78e73b06f671bd68944ed81e2ad8
Jonathan Cheng <36514601+epicfailname@users.noreply.github.com>
2019-01-27 17:32:26 +0800

    Added sort command feature.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -14,14 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 /*
  * NOTE : =============================================================
@@ -592,6 +585,7 @@ public class AddressBook {
     }
 
     private static ArrayList<String[]> sort(ArrayList<String[]> persons) {
+        persons.sort(Comparator.comparing(person -> person[0]));
         return persons;
     }
 
